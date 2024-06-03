3d62b947cdf4ad3fb4f644b7b4cd78c3753ff5b7
Arash Farr <arash.moohan+huggingface@gmail.com>
2024-06-02 22:07:26 -0500

    opening video works, tested build on another machine

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 
-int main() {
+// wtf are files the issue here???
+// #include <fstream>
+// #include <string>
+
+// using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv) {
   // Print OpenCV build information
-  std::cout << cv::getBuildInformation() << std::endl;
+  // std::cout << cv::getBuildInformation() << std::endl;
+
+  if (argc < 2) {
+    std::cerr << "give file name" << std::endl;
+    return -1;
+  }
 
   // Open the default camera
-  cv::VideoCapture cap(0);
+  cv::VideoCapture cap(argv[1]);
 
   if (!cap.isOpened()) {
-    std::cerr << "Error: Could not open camera" << std::endl;
+    std::cerr << "Error: Could not open video file" << std::endl;
     return -1;
   }
 
@@ -26,6 +41,24 @@ int main() {
     if (cv::waitKey(30) >= 0)
       break;
   }
+  cap.release();
+  cv::destroyAllWindows();
+
+  // open image
+  // Mat img = imread(argv[1], IMREAD_COLOR);
+  // if(img.empty()) {
+  //   std::cout << "could not open image" << std::endl;
+  //   return -1;
+  // }
+  // imshow("title", img);
+  // cv::waitKey(0);
+
+  // just read a fucking file
+  // std::ifstream file(argv[1]);
+  // std::string str;
+  // while (std::getline(file, str)) {
+  //   std::cout << str << std::endl;
+  // }
 
   return 0;
 }
